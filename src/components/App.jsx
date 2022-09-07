eb52e40f7f5940165f1a2427ff3e9a979faa0102
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-07 11:36:46 +0100

    refactor: Align project to LocationDetails walkthrough
    - index.js: Renames data.json to forecast.json & updates import statement
    - index.js: Passes forecasts.location to App component
    - App.jsx: Destructures/validates location prop & ammends value passed to location prop of LocationDetails

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,37 +3,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
 
-function App({ data }) {
+function App({ location }) {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <LocationDetails location={data.location} />
+      <LocationDetails location={location} />
     </div>
   );
 }
 
 App.propTypes = {
-  data: PropTypes.shape({
-    location: PropTypes.shape({
-      city: PropTypes.string,
-      country: PropTypes.string,
-    }),
-    forecasts: PropTypes.arrayOf(
-      PropTypes.shape({
-        date: PropTypes.number,
-        temperature: PropTypes.shape({
-          max: PropTypes.number,
-          min: PropTypes.number,
-        }),
-        wind: PropTypes.shape({
-          speed: PropTypes.number,
-          direction: PropTypes.string,
-        }),
-        humidity: PropTypes.number,
-        description: PropTypes.string,
-        icon: PropTypes.string,
-      })
-    ),
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
   }).isRequired,
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import data from "./data/data.json";
+import forecast from "./data/forecast.json";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App data={data} />
+    <App location={forecast.location} />
   </React.StrictMode>
 );
