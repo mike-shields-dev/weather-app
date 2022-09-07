1a8a1daa95e992d7d7e038fb14ffef81963a2d30
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-07 12:21:33 +0100

    test: Asserts that App correctly renders LocationDetails

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import App from "../components/App";
+import forecast from "../data/forecast.json";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders correctly", () => {
+    const { location } = forecast;
+    render(<App location={location} />);
+    const h1Element = screen.getByText(/Manchester, UK/i);
+    expect(h1Element).toBeInTheDocument();
+  });
 });
