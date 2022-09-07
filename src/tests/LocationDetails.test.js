d0d16207c2a2c93895b093dd1b78b29c40f10c8f
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-07 15:59:16 +0100

    refactor: Remove hardcoded test values from test suite

--- src/tests/LocationDetails.test.js
+++ src/tests/LocationDetails.test.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import LocationDetails from "../components/LocationDetails";
+import forecast from "../data/forecast.json";
 
 describe("LocationDetails", () => {
   it("renders the correct city and location props", () => {
-    const [city, country] = ["Manchester", "UK"];
+    const { city, country } = forecast.location;
     const { getByText } = render(
       <LocationDetails city={city} country={country} />
     );
