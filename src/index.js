dcdfa15c1e8e67a53ef3e441163a89f265e10e25
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-07 10:57:02 +0100

    chore: Import data.json into index.js and pass to App component as data prop

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
+import data from "./data/data.json";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={data} />
   </React.StrictMode>
 );
