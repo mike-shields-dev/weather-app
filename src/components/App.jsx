08288585c928908d73e63a9ad633421506736272
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-07 10:59:54 +0100

    feat: App component receives data prop
    - Destructures data prop
    - Validates data prop object with PropTypes
    - Temporarily logs data prop to console

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,39 @@
 import "../styles/App.css";
 import React from "react";
+import PropTypes from "prop-types";
 
 function App({ data }) {
+  console.log({ data });
   return (
     <div className="App">
       <h1>Weather App</h1>
     </div>
   );
 }
+
+App.propTypes = {
+  data: PropTypes.shape({
+    location: PropTypes.shape({
+      city: PropTypes.string,
+      country: PropTypes.string,
+    }),
+    forecasts: PropTypes.arrayOf(
+      PropTypes.shape({
+        date: PropTypes.number,
+        temperature: PropTypes.shape({
+          max: PropTypes.number,
+          min: PropTypes.number,
+        }),
+        wind: PropTypes.shape({
+          speed: PropTypes.number,
+          direction: PropTypes.string,
+        }),
+        humidity: PropTypes.number,
+        description: PropTypes.string,
+        icon: PropTypes.string,
+      })
+    ),
+  }).isRequired,
+};
 
 export default App;
