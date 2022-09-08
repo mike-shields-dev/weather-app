d0a5456ae9c2d54cdeba0d1220286b1e7e871105
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-08 20:38:40 +0100

    refactor: ForecastSummary.test.js
    Adds assertions:
    - 'matches snapshot'
    - 'renders date, description, icon and temperature'
    - 'rendered elements have correct class names'

--- src/tests/ForecastSummary.test.js
+++ src/tests/ForecastSummary.test.js
@@ -7,7 +7,7 @@ const dailyForecast = forecast.forecasts[0];
 const { date, description, icon, temperature } = dailyForecast;
 
 describe("ForecastSummary", () => {
-  it("renders correctly", () => {
+  it("matches snapshot", () => {
     const { asFragment } = render(
       <ForecastSummary
         date={date}
@@ -20,7 +20,7 @@ describe("ForecastSummary", () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it("renders provided props correctly", () => {
+  it("renders date, description, icon and temperature", () => {
     const { getByText, getByTestId } = render(
       <ForecastSummary
         data-testid="forecast-summary"
@@ -31,21 +31,30 @@ describe("ForecastSummary", () => {
       />
     );
 
-    const dateEl = getByText(date);
-    const descriptionEl = getByText(description);
-    const iconEl = getByTestId("forecast-summary-icon");
-    const temperatureEl = getByText(temperature.max);
+    expect(getByText(date)).toBeInTheDocument();
+    expect(getByText(description)).toBeInTheDocument();
+    expect(getByTestId("forecast-summary-icon")).toBeInTheDocument();
+    expect(getByText(temperature.max)).toBeInTheDocument();
+  });
 
-    expect(dateEl).toBeInstanceOf(HTMLHeadingElement);
-    expect(dateEl).toHaveClass("forecast-summary__date");
+  it("rendered elements have correct class names", () => {
+    const { getByText, getByTestId } = render(
+      <ForecastSummary
+        data-testid="forecast-summary"
+        date={date}
+        description={description}
+        icon={icon}
+        temperature={temperature}
+      />
+    );
 
-    expect(descriptionEl).toBeInstanceOf(HTMLParagraphElement);
-    expect(descriptionEl).toHaveClass("forecast-summary__description");
-
-    expect(iconEl).toBeInstanceOf(HTMLParagraphElement);
-    expect(iconEl).toHaveClass("forecast-summary__icon");
-
-    expect(temperatureEl).toBeInstanceOf(HTMLParagraphElement);
-    expect(temperatureEl).toHaveClass("forecast-summary__temperature");
+    expect(getByText(date)).toHaveClass("forecast-summary__date");
+    expect(getByText(description)).toHaveClass("forecast-summary__description");
+    expect(getByTestId("forecast-summary-icon")).toHaveClass(
+      "forecast-summary__icon"
+    );
+    expect(getByText(temperature.max)).toHaveClass(
+      "forecast-summary__temperature"
+    );
   });
 });
