adfcaa04e2731cb8c7905ca11787e496b77e6a4d
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-11 22:20:15 +0100

    style: Add styles to LocationDetails

--- src/components/LocationDetails.jsx
+++ src/components/LocationDetails.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "../styles/LocationDetails.module.css";
 
 export default function LocationDetails({ city, country }) {
   return (
-    <header className="location-details" data-testid="location-details">
+    <header
+      className={styles["location-details"]}
+      data-testid="location-details"
+    >
       <h1 className="location-details__title">
         {city}, {country}
       </h1>
