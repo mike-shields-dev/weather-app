1861a1bc2590032f4c24d307d19c6f8ba18952ef
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-12 16:03:43 +0100

    test/refactor: Add validDate variable to remove dependency on forecast.json

--- src/tests/components/FormattedDate.test.js
+++ src/tests/components/FormattedDate.test.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import FormattedDate from "../../components/FormattedDate";
-import { forecasts } from "../../data/forecast.json";
 import unixTimestampToFormattedDate from "../../helpers/unixTimestampToFormattedDate";
 
-const { date } = forecasts[0];
-const formattedDate = unixTimestampToFormattedDate(date);
+const validDate = 1525046400000;
+const formattedDate = unixTimestampToFormattedDate(validDate);
 
 describe("FormattedDate", () => {
   it("matches snapshot", () => {
-    const { asFragment } = render(<FormattedDate date={date} />);
+    const { asFragment } = render(<FormattedDate date={validDate} />);
 
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders date in a human readable format", () => {
-    const { getByText } = render(<FormattedDate date={date} />);
+    const { getByText } = render(<FormattedDate date={validDate} />);
 
     expect(getByText(formattedDate)).toBeInTheDocument();
   });
