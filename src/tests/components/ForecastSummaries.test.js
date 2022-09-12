bf9b02f5883912ac5fb5b4e14bb7cd96375dab3c
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-12 16:09:48 +0100

    test/refactor: ForecastSummaries.test.js
    - Adds validProps array to remove dependency on forecast.json
    - Updates snapshot

--- src/tests/components/ForecastSummaries.test.js
+++ src/tests/components/ForecastSummaries.test.js
@@ -1,15 +1,57 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import ForecastSummaries from "../../components/ForecastSummaries";
-import forecast from "../../data/forecast.json";
 
-const { forecasts } = forecast;
+const validProps = [
+  {
+    date: 1525046400000,
+    temperature: {
+      max: 11,
+      min: 4,
+    },
+    wind: {
+      speed: 13,
+      direction: "s",
+    },
+    humidity: 30,
+    description: "Clear",
+    icon: "800",
+  },
+  {
+    date: 1525132800000,
+    temperature: {
+      max: 13,
+      min: 8,
+    },
+    wind: {
+      speed: 60,
+      direction: "ne",
+    },
+    humidity: 80,
+    description: "Stormy",
+    icon: "211",
+  },
+  {
+    date: 1525219200000,
+    temperature: {
+      max: 1,
+      min: -2,
+    },
+    wind: {
+      speed: 5,
+      direction: "n",
+    },
+    humidity: 50,
+    description: "Heavy Snow",
+    icon: "602",
+  },
+];
 
 describe("ForecastSummaries", () => {
   it("matches snapshot", () => {
     const { asFragment } = render(
       <ForecastSummaries
-        forecasts={forecasts}
+        forecasts={validProps}
         handleForecastSelect={jest.fn()}
       />
     );
@@ -19,10 +61,10 @@ describe("ForecastSummaries", () => {
   it("renders forecasts as ForecastSummary components", () => {
     const { getAllByTestId } = render(
       <ForecastSummaries
-        forecasts={forecasts}
+        forecasts={validProps}
         handleForecastSelect={jest.fn()}
       />
     );
-    expect(getAllByTestId("forecast-summary")).toHaveLength(forecasts.length);
+    expect(getAllByTestId("forecast-summary")).toHaveLength(validProps.length);
   });
 });
