3f27f1788090165fe7cca58b130d70e107964987
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-12 16:28:49 +0100

    test/refactor: Add date variable to remove forecast.json dependency

--- src/tests/helpers/unixTimestampToFormattedDate.test.js
+++ src/tests/helpers/unixTimestampToFormattedDate.test.js
@@ -1,10 +1,9 @@
 import unixTimestampToFormattedDate from "../../helpers/unixTimestampToFormattedDate";
-import { forecasts } from "../../data/forecast.json";
 
 describe("unixTimestampToFormattedDate", () => {
   it("returns human readable date when give unixTimestamp", () => {
-    const { date: timestamp } = forecasts[0];
+    const date = 1525046400000;
 
-    expect(unixTimestampToFormattedDate(timestamp)).toEqual("Mon 30th Apr");
+    expect(unixTimestampToFormattedDate(date)).toEqual("Mon 30th Apr");
   });
 });
