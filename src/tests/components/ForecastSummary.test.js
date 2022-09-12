a2eb3c8748ef5c240b8de38dde8bf5a98a19dbd7
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-12 16:18:35 +0100

    test/refactor: Add validProps object to remove forecast.json dependency

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -1,29 +1,37 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import ForecastSummary from "../../components/ForecastSummary";
-import forecast from "../../data/forecast.json";
 import unixTimestampToHumanFormattedDate from "../../helpers/unixTimestampToFormattedDate";
 
-const dailyForecast = forecast.forecasts[0];
-const { date, description, temperature } = dailyForecast;
+const validProps = {
+  date: 1525046400000,
+  temperature: {
+    max: 11,
+    min: 4,
+  },
+  wind: {
+    speed: 13,
+    direction: "s",
+  },
+  humidity: 30,
+  description: "Clear",
+  icon: "800",
+  handleForecastSelect: jest.fn(),
+};
+
+const { date, description, temperature } = validProps;
 const formattedDate = unixTimestampToHumanFormattedDate(date);
 
 describe("ForecastSummary", () => {
   it("matches snapshot", () => {
-    const { asFragment } = render(
-      <ForecastSummary handleForecastSelect={jest.fn()} {...dailyForecast} />
-    );
+    const { asFragment } = render(<ForecastSummary {...validProps} />);
 
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders formatted date, description, icon and temperature", () => {
     const { getByText, getByTestId } = render(
-      <ForecastSummary
-        data-testid="forecast-summary"
-        handleForecastSelect={jest.fn()}
-        {...dailyForecast}
-      />
+      <ForecastSummary data-testid="forecast-summary" {...validProps} />
     );
 
     expect(getByText(formattedDate)).toBeInTheDocument();
@@ -33,20 +41,13 @@ describe("ForecastSummary", () => {
   });
 
   it("More details... button should call handleForecastSelect callback", () => {
-    const handleForecastSelect = jest.fn();
-
     const { getByText } = render(
-      <ForecastSummary
-        data-testid="forecast-summary"
-        handleForecastSelect={handleForecastSelect}
-        {...dailyForecast}
-      />
+      <ForecastSummary data-testid="forecast-summary" {...validProps} />
     );
-
     const detailsButton = getByText("More details...");
 
     fireEvent.click(detailsButton);
 
-    expect(handleForecastSelect).toHaveBeenCalled();
+    expect(validProps.handleForecastSelect).toHaveBeenCalled();
   });
 });
