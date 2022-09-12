f628b39c15fbff9132bc76fa9571b0a3adfc55f8
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-12 16:22:55 +0100

    test/refactor: LocationDetails.test.js
    - Add validProps object to remove dependency on forecast.json
    Updates snapshot

--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -1,30 +1,29 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import LocationDetails from "../../components/LocationDetails";
-import forecast from "../../data/forecast.json";
+
+const validProps = {
+  city: "Lancaster",
+  country: "GB",
+};
+const { city, country } = validProps;
 
 describe("LocationDetails", () => {
-  const { city, country } = forecast.location;
-
   it("matches snapshot", () => {
-    const { asFragment } = render(
-      <LocationDetails city={city} country={country} />
-    );
+    const { asFragment } = render(<LocationDetails {...validProps} />);
 
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders city and country", () => {
-    const { getByText } = render(
-      <LocationDetails city={city} country={country} />
-    );
+    const { getByText } = render(<LocationDetails {...validProps} />);
+
     expect(getByText(`${city}, ${country}`)).toBeInTheDocument();
   });
 
   it("has correct css class name", () => {
-    const { getByText } = render(
-      <LocationDetails city={city} country={country} />
-    );
+    const { getByText } = render(<LocationDetails {...validProps} />);
+
     expect(getByText(`${city}, ${country}`)).toHaveClass(
       "location-details__title"
     );
